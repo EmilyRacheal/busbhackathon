398173a2cf5788050cf71ad2d210b2eb1336af68
emily <emilychealy4@gmail.com>
2022-11-14 17:53:01 +0100

    working on the side nav

--- src/Components/Layout/Sidebar/sidenav.jsx
+++ src/Components/Layout/Sidebar/sidenav.jsx
@@ -5,8 +5,8 @@ import {MdOutlineAdd} from "react-icons/md"
 
 function Sidenav() {
   return (
-    <div  className='p-4 w-[100%] max-w-[500px] shadow-lg'>
-        <div className='flex flex-col gap-y-5'>
+    <div  className='p-4 w-[100%] max-w-[400px] shadow-lg'>
+        <div className='flex flex-col gap-y-5 w-full justify-center items-center'>
         <div className='w-[100%] max-w-[300px] border py-10 border-gray-400 flex flex-col justify-center items-center'>
                 <img src={Userimage} className="w-[100px] h-[100px] rounded-[50%]" alt="" srcset="" />
             <div className='flex items-center gap-x-2'>
@@ -24,11 +24,11 @@ function Sidenav() {
             <div className='flex-col flex gap-y-2 px-5'>
                 <h1 className='text-center text-[#FFA945] text-[20px] font-semibold'>Posted Jobs</h1>
                 <h3 className='text-center font-medium text-[16px]'>Program Manager at Lutech</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent  </p>
+                <p className='text-[13px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent  </p>
             </div>
             <div className='border border-b-0 border-[#979797] w-[90%] my-5'/>
             <div className='px-5'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent  </p>
+            <p className='text-[13px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent  </p>
 
             </div>
         </div>
